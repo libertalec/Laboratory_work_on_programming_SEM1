d357cffe85f68ee227a049d8385145fe0527a3e8
slavii <staskrutvk@mail.ru>
2021-12-02 23:53:21 +0300

    change 7-8

--- lab6.c
+++ lab6.c
@@ -1,23 +1,74 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <math.h>
+
+enum MONTHS {
+    JANUARY = 1,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+
+};
+
+struct coordinates {
+    int x;
+    int y;
+};
+
+struct rectangle // Прямоугольник
+{
+    struct coordinates A;
+    struct coordinates B;
+};
+
+union Printer {
+    struct laser_printer {
+        unsigned elements : 1; //Отводим по одному биту на состояния в структуре
+        unsigned low_toner : 1;
+        unsigned broken : 1;
+        unsigned no_paper : 1;
+    } stats;
+    unsigned input; //Объединяем состояния с input
+} printer;
 
 int main() {
-    printf("First task: \n");
-    double mas[5] = {56.4, 36.5,7.7,44.3};
-    for (int i = 0; i < 4; i++) {
-        printf("%f\n", *(mas + i));
-    }
+    printf("%s\n", "===Task 1===");
+    enum MONTHS j;
+    j = JULY;
+    printf("Months: %d\n\n", j);
 
-    printf("\nSecond task: \n");
-    double *arr = (double *) malloc(4 * sizeof(double));
-    arr[0] = 56.4;
-    arr[1] = 36.5;
-    arr[2] = 7.7;
-    arr[3] = 44.3;
-    for (int i = 0; i < 4; i++) {
-        printf("%f\n", *(arr + i));
-    }
-    free(arr);
+    printf("%s\n", "===Task 2===");
+    struct rectangle Rectangle;
+    Rectangle.A.x = 0;
+    Rectangle.A.y = 0;
+
+    Rectangle.B.x = 0;
+    Rectangle.B.y = 2;
+
+
+    double AB = sqrt(pow(Rectangle.A.x - Rectangle.B.x, 2) + pow(Rectangle.A.y - Rectangle.B.y, 2));
+
+
+    double perimeter;
+    perimeter = AB * 4;
+    printf("%f\n",perimeter);
+
+    printf("%s\n", "===Task 3===");
+
+    scanf("%x", &printer.input);
+    printf("Elements: %s\n", printer.stats.elements ? "ON" : "OFF");
+    printf("Little toner: %s\n", printer.stats.low_toner ? "ON" : "OFF");
+    printf("Broken: %s\n", printer.stats.broken ? "ON" : "OFF");
+    printf("No paper: %s\n", printer.stats.no_paper ? "ON" : "OFF");
+
     return 0;
 }
-
--- lab8.c
+++ lab8.c
@@ -3,72 +3,64 @@
 #include <stdlib.h>
 #include <math.h>
 
-enum MONTHS {
-    JANUARY = 1,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
-
-};
-
-struct coordinates {
-    int x;
-    int y;
-};
-
-struct rectangle // Прямоугольник
-{
-    struct coordinates A;
-    struct coordinates B;
-};
-
-union Printer {
-    struct laser_printer {
-        unsigned elements : 1; //Отводим по одному биту на состояния в структуре
-        unsigned low_toner : 1;
-        unsigned broken : 1;
-        unsigned no_paper : 1;
-    } stats;
-    unsigned input; //Объединяем состояния с input
-} printer;
 
 int main() {
-    printf("%s\n", "===Task 1===");
-    enum MONTHS j;
-    j = JULY;
-    printf("Months: %d\n\n", j);
-
-    printf("%s\n", "===Task 2===");
-    struct rectangle Rectangle;
-    Rectangle.A.x = 0;
-    Rectangle.A.y = 0;
-
-    Rectangle.B.x = 0;
-    Rectangle.B.y = 2;
-
-
-    double AB = sqrt(pow(Rectangle.A.x - Rectangle.B.x, 2) + pow(Rectangle.A.y - Rectangle.B.y, 2));
+    int k = 50;
+    printf("%s\n", "==== Task 2 ====");
+    char *str1 = (char *) malloc(sizeof(char) * k);
+    char *str2 = (char *) malloc(sizeof(char) * k);
+    scanf("%s", str1);
+    scanf("%s", str2);
+    int x;
+    scanf("%d", &x);
+    char *temp = (char *) malloc(sizeof(char) * x);
+    strncpy(temp, str2, x); //temp – первые x символов со второй строки
+    strcat(str1, temp); //Объединяем первую строку и первые x символов второй строки
+    printf("%s\n", str1);
 
 
-    double perimeter;
-    perimeter = AB * 4;
-    printf("%f\n",perimeter);
+    printf("%s\n", "==== Task 4 ====");
+    int n;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        if(str1[i]==str2[i]){
+            printf("The letter with the number i matched\n");
+        }else {
+            printf("The lines don't match\n");
+            break;
+        }
+    }
 
-    printf("%s\n", "===Task 3===");
+    printf("%s\n", "==== Task 6 ====");
+    int d;
+    scanf("%d", &d);
+    char *temp1 = (char *) malloc(sizeof(char) * d);
+    strncpy(temp1, str2, d);
+    str1=temp1;
+    printf("%s\n", str1);
 
-    scanf("%x", &printer.input);
-    printf("Elements: %s\n", printer.stats.elements ? "ON" : "OFF");
-    printf("Little toner: %s\n", printer.stats.low_toner ? "ON" : "OFF");
-    printf("Broken: %s\n", printer.stats.broken ? "ON" : "OFF");
-    printf("No paper: %s\n", printer.stats.no_paper ? "ON" : "OFF");
+    printf("%s\n", "==== Task 8 ====");
+    char val;
+    scanf("%s", &val);
+    char *ach;
+
+    ach = strchr(str1, val);
+
+    // Выводим результат на консоль
+    if (ach == NULL) {
+        printf("The desired symbol was not found\n");
+    } else {
+        printf("%d\n", ach - str1 + 1);
+    }
+
+    printf("%s\n", "==== Task 10 ====");
+    char *p;
+    for (p = str1; *p; ++p) {
+        if (strchr(str2, *p)) {
+            printf("found %c\n", *p);
+        }
+    }
+
 
     return 0;
 }
